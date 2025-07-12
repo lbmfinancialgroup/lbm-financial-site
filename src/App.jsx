6acfa663874a39dede5add94ca8d119876105199
Lacey Allen <lbmfinancialgroup@gmail.com>
2025-07-11 17:14:09 -0700

    Initial full project commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '5rem' }}>
+      <h1>LBM Financial Group</h1>
+      <p>Smarter Finance. Stronger Futures.</p>
+    </div>
+  )
+}
